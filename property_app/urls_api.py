1c9981012ebc9560281bf8857bdffb6f424d736d
Libardo Cuello <libardoii@hotmail.com>
2024-06-27 17:11:30 -0500

    add validations to form

--- property_app/urls_api.py
+++ property_app/urls_api.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from property_app.views import PropertyApiView, get_countries, get_property_type
+from property_app.views import PropertyApiView, get_property_type
 
 urlpatterns = [
     path("types/property/", get_property_type),
     path("property/", PropertyApiView.as_view()),
     path("property/<int:id>/", PropertyApiView.as_view()),
-    path("countries/", get_countries),
 ]
