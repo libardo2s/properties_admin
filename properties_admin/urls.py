1c9236bfec5429f48131428d25d30bbfc44a8538
Libardo Cuello <libardoii@hotmail.com>
2024-06-27 15:48:59 -0500

    crud complete in template

--- properties_admin/urls.py
+++ properties_admin/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("property_app.urls_api")),
     path("", include("property_app.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- property_app/views.py
+++ property_app/views.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+from django.conf import settings
 from django.shortcuts import render
 
 from rest_framework import status
@@ -58,7 +62,7 @@ class PropertyApiView(APIView):
     def get(self, request, id=None, format=None):
         try:
 
-            properties_list = Property.objects.filter(deleted=False)
+            properties_list = Property.objects.filter(deleted=False).order_by("id")
             properties_serializer = PropertySerializer(properties_list, many=True)
 
             response = {
@@ -110,21 +114,32 @@ class PropertyApiView(APIView):
             property = Property.objects.get(id=id)
             property_serializer = PropertySerializer(data=request.data)
             if property_serializer.is_valid():
-                property.name = property_serializer.get("name", property.name)
-                property.address = property_serializer.get("address", property.address)
-                property.country = property_serializer.get("country", property.country)
-                property.city = property_serializer.get("city", property.city)
-                property.zip_code = property_serializer.get(
+                property_serializer_data = property_serializer.data
+                property.name = property_serializer_data.get("name", property.name)
+                property.address = property_serializer_data.get(
+                    "address", property.address
+                )
+                property.country = property_serializer_data.get(
+                    "country", property.country
+                )
+                property.city = property_serializer_data.get("city", property.city)
+                property.zip_code = property_serializer_data.get(
                     "zip_code", property.zip_code
                 )
-                property.type = property_serializer.get("type", property.type.id)
-                property.area = property_serializer.get("area", property.area)
+                property.area = property_serializer_data.get("area", property.area)
+
+                if property_serializer_data.get("type"):
+                    type_property = PropertyType.objects.get(
+                        id=property_serializer_data.get("type")
+                    )
+                    property.type = type_property
 
                 property.save()
 
+                property_serializer = PropertySerializer(property, many=False)
                 response = {
                     "message": "Propiedad actualizada correctamente",
-                    "content": {},
+                    "content": property_serializer.data,
                 }
 
                 return Response(response, status=status.HTTP_200_OK)
